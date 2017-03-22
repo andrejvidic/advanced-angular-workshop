56101d029cc74b6e6330f5323cf58a8479ee4f26
Andre Vidic <andre@dtcorp.com.au>
2017-03-22 21:33:46 +1100

    activity-list-component: create component.ts

---
+++ src/app/activity-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-list',
+  template: `
+    <p>
+      activity-list Works!
+    </p>
+  `,
+  styles: []
+})
+export class ActivityListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
